ccb23d1052976f69b763be7151464f5f34827674
Michel Romero <studentenherz@gmail.com>
2020-08-28 20:46:37 -0300

    title and maxwidth for info strip

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-infoStrip = document.getElementById("info-container");
+infoStrip = document.getElementsByTagName("header")[0];
+coronaBariloche = document.querySelector("header h1");
 console.log(infoStrip);
 
 window.onscroll = function () {
   var offset = window.pageYOffset;
   var lightness = 89;
+  var fontsize = 36;
   if (offset <= 100) {
     lightness = 100 - offset * 0.21;
+    fontsize = 36 - offset * 0.14;
     infoStrip.style.padding = `${(100 - offset)*0.3}px 0`;
+    coronaBariloche.style.fontSize = `${fontsize}px`;
   }
 
   infoStrip.style.boxShadow = `hsl(0, 0%, ${lightness}%) 5px 5px 10px`;
