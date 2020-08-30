948d2d35b7f9767d457c98f6a91eaddad8f2e3a5
Michel Romero <studentenherz@gmail.com>
2020-08-30 02:19:00 -0300

    added references and maintainance strip

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 infoStrip = document.getElementsByTagName("header")[0];
 coronaBariloche = document.querySelector("header h1");
-console.log(infoStrip);
 
 window.onscroll = function () {
   var offset = window.pageYOffset;
   var lightness = 89;
   var fontsize = 36;
+  var shade = 10;
   if (offset <= 100) {
     lightness = 100 - offset * 0.21;
     fontsize = 36 - offset * 0.14;
-    infoStrip.style.padding = `${(100 - offset)*0.3}px 0`;
+    infoStrip.style.padding = `${(100 - offset) * 0.3}px 0`;
     coronaBariloche.style.fontSize = `${fontsize}px`;
+    shade = 0.1 * offset;
   }
 
-  infoStrip.style.boxShadow = `hsl(0, 0%, ${lightness}%) 5px 5px 10px`;
-  
+  infoStrip.style.boxShadow = `hsl(0, 0%, ${lightness}%) 5px 5px ${shade}px`;
+
 
   //   hsl(34, 26%, 89%)
 }
