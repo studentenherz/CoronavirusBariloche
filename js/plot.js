6c31d13b708bfa72d581d8759800a3e2e6b69fed
Michel Romero <studentenherz@gmail.com>
2020-09-02 12:13:45 -0300

    update data

--- js/plot.js
+++ js/plot.js
@@ -5,12 +5,13 @@ function drawChart() {
 	var n = dataArray.length;
 
 	// make accumulated sums 
-	// for (var i = 1; i < dataArray.length; ++i) {
-	// 	dataArray[i][3] = dataArray[i - 1][3] + dataArray[i][2];
-	// 	dataArray[i][5] = dataArray[i - 1][5] + dataArray[i][4];
-	// 	dataArray[i][7] = dataArray[i - 1][7] + dataArray[i][6];
-	// 	dataArray[i][8] = dataArray[i - 1][8] + dataArray[i][2] - dataArray[i][4] - dataArray[i][6];
-	// }
+	for (var i = 1; i < dataArray.length; ++i) {
+		if (dataArray[i][3] == null)
+			dataArray[i][3] = dataArray[i - 1][3] + dataArray[i][2];
+		// dataArray[i][5] = dataArray[i - 1][5] + dataArray[i][4];
+		// dataArray[i][7] = dataArray[i - 1][7] + dataArray[i][6];
+		// dataArray[i][8] = dataArray[i - 1][8] + dataArray[i][2] - dataArray[i][4] - dataArray[i][6];
+	}
 
 	document.getElementById("day").innerHTML = dataArray[n - 1][0];
 	document.getElementById("date").innerHTML = `(${dataArray[n - 1][1]})`;
@@ -139,8 +140,8 @@ function drawChart() {
 		},
 		hAxis: {
 			viewWindow: {
-				min: 0,
-				max: 150,
+				min: -1,
+				max: dataArray[n - 1][0] + 1,
 			},
 			title: 'Día desde el inicio del brote en Bariloche (04/03/20)',
 			titleTextStyle: {
@@ -481,6 +482,7 @@ const dataArray = [
 	[147, "08/27/20", 34, 1501, 23, 1090, 1, 28, 385],
 	[148, "08/28/20", 3, 1504, 47, 1137, 0, 28, 277],
 	[149, "08/29/20", 23, 1527, 62, 1200, 0, 28, 290],
-	[150, "08/30/20", 3, 1530, 10, 1210, 0, 28, 283],
-	[151, "08/31/20", 37, 1567, 26, 1335, 1, 29, 237]
+	[150, "08/30/20", 3, null, 10, 1210, 0, 28, 283],
+	[151, "08/31/20", 37, null, 26, 1335, 1, 29, 237],
+	[152, "09/01/20", 28, null, 21, 1358, 0, 29, 223]
 ];
